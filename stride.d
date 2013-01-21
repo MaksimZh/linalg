4f742e024f8b366e3d26660c4b0f3e94a064c77f
Maksim Zholudev <maximzms@gmail.com>
2013-01-21 12:48:50 +0100

    Added function for transposed dense array

--- stride.d
+++ stride.d
@@ -8,7 +8,7 @@
 */
 module stride;
 
-// Calculates strides in data array
+// Calculates strides in data array for dense storage
 size_t[] calcDenseStrides(const(size_t)[] dim) pure
 {
     if(dim.length == 1)
@@ -24,4 +24,22 @@ unittest
 {
     assert(calcDenseStrides([2, 3, 4]) == [12, 4, 1]);
     static assert(calcDenseStrides([2, 3, 4]) == [12, 4, 1]);
+}
+
+// Calculates strides in data array for transposed dense storage
+size_t[] calcDenseStridesTransp(const(size_t)[] dim) pure
+{
+    if(dim.length == 1)
+        return [1];
+    else
+    {
+        auto tail = calcDenseStridesTransp(dim[0..$-1]);
+        return tail ~ [dim[$-2] * tail[$-1]];
+    }
+}
+
+unittest
+{
+    assert(calcDenseStridesTransp([2, 3, 4]) == [1, 2, 6]);
+    static assert(calcDenseStridesTransp([2, 3, 4]) == [1, 2, 6]);
 }
