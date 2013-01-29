65463ac300719c5a87775db7b4cbd31414fa2e1e
Maksim Zholudev <maximzms@gmail.com>
2013-01-29 14:52:53 +0100

    Added slice proxy mixin

--- containers.d
+++ containers.d
@@ -64,7 +64,6 @@ mixin template storage(ElementType, alias dimPattern,
     else
         public @property size_t[rank] dimensions() pure const { return _dim; }
 
-
     /* Change dimensions */
     static if(storageType == StorageType.resizeable)
     {
@@ -110,5 +109,158 @@ mixin template storage(ElementType, alias dimPattern,
             }
             _resize();
         }
+    }
+}
+
+/* Structure to store slice boundaries compactly */
+private struct SliceBounds
+{
+    size_t lo;
+    size_t up;
+
+    this(size_t lo_, size_t up_)
+    {
+        lo = lo_;
+        up = up_;
+    }
+
+    this(size_t i)
+    {
+        lo = i;
+        up = i;
+    }
+
+    // Whether this is regular slice or index
+    bool isRegularSlice()
+    {
+        return !(lo == up);
+    }
+}
+
+mixin template sliceProxy(SourceType, alias constructSlice)
+{
+    /* Auxiliary structure for slicing and indexing */
+    struct SliceProxy(size_t sliceRank, size_t sliceDepth)
+    {
+        SliceBounds[] bounds; //FIXME: dynamic array is not an optimal solution
+
+        SourceType* source; // Pointer to the container being sliced
+
+        private this(SourceType* source_, SliceBounds[] bounds_)
+        {
+            source = source_;
+            bounds = bounds_;
+        }
+
+        /* Evaluate slicing result */
+        static if(sliceRank > 0)
+        {
+            auto eval()
+            {
+                static if(depth < rank)
+                {
+                    /* If there is not enough bracket pairs - add empty [] */
+                    static if(depth == rank - 1)
+                        return this[];
+                    else
+                        return this[].eval();
+                }
+                else
+                {
+                    /* Normal slice */
+                    return constructSlice(*source, bounds);
+                }
+            }
+        }
+        else
+        {
+            /* If simple index return element by reference */
+            ref auto eval()
+            {
+                size_t index = 0; // Position in the container
+                foreach(i, b; bounds)
+                    index += source._stride[i] * b.lo;
+                return source._container[index];
+            }
+        }
+
+        /* Slicing and indexing */
+        static if(depth < dimPattern.length - 1)
+        {
+            /* Return slice proxy for incomplete bracket construction
+            */
+            SliceProxy!(sliceRank, depth + 1) opSlice()
+            {
+                return typeof(return)(
+                    source, bounds ~ SliceBounds(0, source._dim[depth]));
+            }
+
+            SliceProxy!(sliceRank, depth + 1) opSlice(size_t lo, size_t up)
+            {
+                return typeof(return)(source, bounds ~ SliceBounds(lo, up));
+            }
+
+            SliceProxy!(sliceRank - 1, depth + 1) opIndex(size_t i)
+            {
+                return typeof(return)(source, bounds ~ SliceBounds(i));
+            }
+        }
+        else static if(depth == (dimPattern.length - 1))
+             {
+                 /* If only one more slicing can be done
+                    then return slice not proxy
+                 */
+                 auto opSlice()
+                 {
+                     return SliceProxy!(sliceRank, depth + 1)(
+                         source, bounds ~ SliceBounds(0, source._dim[depth])
+                         ).eval();
+                 }
+
+                 auto opSlice(size_t lo, size_t up)
+                 {
+                     return SliceProxy!(sliceRank, depth + 1)(
+                         source, bounds ~ SliceBounds(lo, up)).eval();
+                 }
+
+                 static if(sliceRank > 1)
+                 {
+                     auto opIndex(size_t i)
+                     {
+                         return SliceProxy!(sliceRank - 1, depth + 1)(
+                             source, bounds ~ SliceBounds(i)).eval();
+                     }
+                 }
+                 else
+                 {
+                     /* If simple index return element by reference */
+                     ref auto opIndex(size_t i)
+                     {
+                         return SliceProxy!(sliceRank - 1, depth + 1)(
+                             source, bounds ~ SliceBounds(i)).eval();
+                     }
+                 }
+             }
+
+        auto opCast(Tresult)()
+        {
+            return cast(Tresult)(eval());
+        }
+    }
+
+    /* Slicing and indexing */
+    SliceProxy!(rank, 1) opSlice()
+    {
+        return typeof(return)(&this, [SliceBounds(0, _dim[0])]);
+    }
+
+    SliceProxy!(rank, 1) opSlice(size_t lo, size_t up)
+    {
+        return typeof(return)(&this, [SliceBounds(lo, up)]);
+    }
+
+    SliceProxy!(rank - 1, 1) opIndex(size_t i)
+    {
+        return typeof(return)(&this, [SliceBounds(i)]);
     }
 }
