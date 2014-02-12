1f44ab2c54cc8ce03c21a448af6bf78f4d8daf67
Maksim Zholudev <maximzms@gmail.com>
2014-02-12 18:19:45 +0400

    Test case

--- linalg/test.d
+++ linalg/test.d
@@ -1,11 +1,18 @@
-module test;
-
 import std.traits;
 
 template TypeOfOp(Tlhs, string op, Trhs)
 {
     alias ReturnType!((Tlhs lhs, Trhs rhs) => mixin("lhs"~op~"rhs"))
         TypeOfOp;
+}
+
+
+template TypeOfOp1(Tlhs, string op, Trhs)
+{
+    static if (is(FunctionTypeOf!((Tlhs lhs, Trhs rhs) =>
+                                  mixin("lhs"~op~"rhs")) R
+                  == return))
+        alias R TypeOfOp;
 }
 
 void copy(Tsrc, Tdest)(Tsrc src, Tdest dest)
