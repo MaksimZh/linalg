f8142815300610364462746b68101feb8594f0b6
Maksim Zholudev <maximzms@gmail.com>
2014-02-12 17:35:38 +0400

    Refining

--- linalg/matrix.d
+++ linalg/matrix.d
@@ -15,127 +15,70 @@ import std.traits;
 
 import oddsends;
 
-import linalg.storage.regular1d;
 import linalg.storage.regular2d;
-import linalg.storage.slice;
-import linalg.operations.basic;
-import linalg.operations.conjugation;
-import linalg.operations.multiplication;
-import linalg.operations.eigen;
-import linalg.operations.inversion;
+import linalg.aux.traits;
 
-debug import linalg.aux.debugging;
-
-version(unittest)
+void copy(Tsource, Tdest)(auto ref Tsource source,
+                          auto ref Tdest dest) pure
 {
-    import std.array;
-    import std.range;
+    
+    static assert(isExistOp!(Tdest.ElementType, "=", Tsource.ElementType),
+                  "Cannot copy (" ~ Tsource.ElementType.stringof
+                  ~ ") to (" ~ Tdest.ElementType.stringof ~ ")");
+    auto isource = source.byElement;
+    auto idest = dest.byElement;
+    while(!(isource.empty))
+    {
+        idest.front = isource.front;
+        isource.popFront();
+        idest.popFront();
+    }
 }
 
-alias linalg.storage.slice.Slice Slice; //NOTE: waiting for proper slice support
-
-
-/* Derive storage type for given matrix parameters */
-private template MatrixStorageType(T, StorageOrder storageOrder,
-                                   size_t nrows, size_t ncols)
+void conjMatrix(Tsource, Tdest)(
+    auto ref Tsource source, auto ref Tdest dest) pure
 {
-    static if(nrows == 1)
-        alias StorageRegular1D!(T, ncols) MatrixStorageType;
-    else static if(ncols == 1)
-        alias StorageRegular1D!(T, nrows) MatrixStorageType;
-    else
-        alias StorageRegular2D!(T, storageOrder, nrows, ncols)
-            MatrixStorageType;
+    static if(Tsource.rank == 1)
+    {
+        static if(isComplex!(Tsource.ElementType))
+            map!("a.conj")(source, dest);
+        else
+            copy(source, dest);
+    }
+    else static if(Tsource.rank == 2)
+    {
+        auto isource = source.byRow();
+        auto idest = dest.byCol();
+        while(!(isource.empty))
+        {
+            conjMatrix(isource.front, idest.front);
+            isource.popFront();
+            idest.popFront();
+        }
+    }
 }
 
-/**
- * Memory management of matrix or vector
- */
-enum MatrixMemory
-{
-    stat,
-    bound,
-    dynamic
-}
-
-/**
- * Matrix or vector or view.
- */
 struct BasicMatrix(T)
 {
     enum size_t nrows_ = 2;
     enum size_t ncols_ = 2;
-    enum size_t[2] dimPattern = [nrows_, ncols_];
 
     enum StorageOrder storageOrder = defaultStorageOrder;
-    alias MatrixStorageType!(T, storageOrder, nrows_, ncols_)
-        StorageType;
-    alias StorageType.ElementType ElementType;
-    enum MatrixMemory memoryManag =
-        StorageType.isStatic ? MatrixMemory.stat : MatrixMemory.dynamic;
-    enum bool isStatic = (memoryManag == MatrixMemory.stat);
+    StorageRegular2D!(T, storageOrder, nrows_, ncols_) storage;
 
-    /**
-     * Storage of matrix data.
-     * This field is public to allow direct access to matrix data storage
-     * if optimization is needed.
-     */
-    public StorageType storage;
-
-    /* Constructors
-     */
-
-    /* Creates matrix for storage. For internal use only.
-     * Public because used by ranges.
-     */
-    this()(auto ref StorageType storage) pure
+    ref auto opAssign(Tsource)(auto ref Tsource source) pure
     {
-        this.storage = storage;
+        copy(source.storage, this.storage);
+        return this;
     }
 
-    @property bool empty() pure const
+    @property ref auto conj() pure
     {
-        static if(isStatic)
-            return false;
-        else
-            return true;
+        //FIXME: Will fail if conjugation changes type
+        BasicMatrix!(T) dest;
+        conjMatrix(this.storage, dest.storage);
+        return dest;
     }
-
-    public // Operations
-    {
-        ref auto opAssign(Tsource)(auto ref Tsource source) pure
-            if(isMatrix!Tsource)
-        {
-            static if(memoryManag == MatrixMemory.dynamic)
-                this.storage = typeof(this.storage)(source.storage);
-            else
-                copy(source.storage, this.storage);
-            return this;
-        }
-    }
-
-    public // Other operations
-    {
-        /**
-         * Return transposed matrix for real matrix or
-         * conjugated and transposed matrix for complex matrix.
-         */
-        @property ref auto conj() pure
-        {
-            //FIXME: Will fail if conjugation changes type
-            BasicMatrix!(ElementType) dest;
-            conjMatrix(this.storage, dest.storage);
-            return dest;
-        }
-    }
-}
-
-/**
- * Detect whether T is matrix
- */
-template isMatrix(T)
-{
-    enum bool isMatrix = isInstanceOf!(BasicMatrix, T);
 }
 
 struct Foo
